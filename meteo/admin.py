a58a0ade012d3ff4062af53723e09b445610a307
AitanaCruxent <aitana.crumar@gmail.com>
2026-06-29 11:46:05 +0200

    user control (Login, Register, Logout..)

--- meteo/admin.py
+++ meteo/admin.py
@@ -1,5 +1,6 @@
 from django.contrib import admin
-from .models import Resort, Summit
+from .models import User, Resort, Summit
 
+admin.site.register(User)
 admin.site.register(Resort)
 admin.site.register(Summit)
--- meteo/urls.py
+++ meteo/urls.py
@@ -3,6 +3,9 @@ from . import views
 
 urlpatterns = [
     path("", views.index, name="index"),
+    path("login", views.login_view, name="login"),
+    path("logout", views.logout_view, name="logout"),
+    path("register", views.register, name="register"),
     path("skiresorts/", views.skiresorts, name="skiresorts"),
     path("summits/", views.summits, name="summits"),
     path("bpa/", views.bpa, name="bpa"),
